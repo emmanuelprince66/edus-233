1c2ac3a8be9904e6462a5906b5546761c8f67fc3
Chukwuemeka Patrick <79291626+djemmydon@users.noreply.github.com>
2024-03-18 08:08:37 +0100

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+<div className=" text-4xl font-extrabold flex justify-center items-center h-screen">
+  <h1>
+  EDUMANAGER 1.0 🚀🚀🚀
+  </h1>
+</div>
+
+  );
+}
